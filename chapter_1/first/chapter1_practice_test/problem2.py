dee1c8d7e3240051babc14526bf29465e4021cd0
adarsh0error <adarshmittal621@gmail.com>
2025-06-05 22:02:44 +0530

    initial commit of python learning codes

---
+++ chapter_1/first/chapter1_practice_test/problem2.py
@@ -0,0 +1,5 @@
+# import an external module and use it to perform a task of your Choice
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("i will speak this text")
+engine.runAndWait()
---
+++ chapter_1/first/jokes_test.py
@@ -0,0 +1,4 @@
+import pyjokes
+print("printing jokes....") 
+jokes = pyjokes.get_joke() #this prints a random joke
+print(jokes) 
