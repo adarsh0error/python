026647227bd113478d59bc6261fe0e7d6da2bb50
adarsh0error <adarshmittal621@gmail.com>
2025-06-06 20:18:56 +0530

    codes related to variables and datatypes

---
+++ chapter_2/rules_variables.py
@@ -0,0 +1,8 @@
+a = 23
+aaa = 435
+harry = 34
+sameer = 45
+_sameer = 56
+#variable can only start with an alphabet or an underscore
+#@sammer = inavalid variable
+#s@ameer #invalid variable
---
+++ chapter_2/variables.py
@@ -0,0 +1,5 @@
+a=1
+b=2
+name = "adarsh"
+print(a+b)
+print(name)
