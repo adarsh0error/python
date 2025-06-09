072ec1eab651056bdd29f74341627891eae9a166
adarsh0error <adarshmittal621@gmail.com>
2025-06-09 13:45:50 +0530

    new changes made

---
+++ chapter_2/operators.py
@@ -0,0 +1,27 @@
+#arithematic operators
+a = 34
+b = 4
+c = a+b
+# print(c)
+
+#assignment operators
+a = 4-2#assign 4-2 in a
+b = 6
+b += 3#increment value of b by 3 and then assign the value to b
+# print(b)
+
+#comparision operaors
+d = 5>4
+e = 5>=5
+f = 5!=7
+g = 5==5
+# print(d)
+# print(e)
+# print(f)
+# print(g)
+
+#logical operators
+h = True or False
+print(h)
+i = True and True
+print(i)
